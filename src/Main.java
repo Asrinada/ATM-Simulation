fb05c7f5f71679e7ff87d56b2510d3d919079e9f
Asrinada <adakolbasi@gmail.com>
2026-01-19 13:05:01 +0300

    Add ATM menu and user choice input

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,16 @@ public class Main {
         boolean calisiyorMu = true;
 
         System.out.println("--EKONOMİ BANKASI ATM'sine HOŞGELDİNİZ--");
+
+        while(calisiyorMu){
+            System.out.println("\n1. Bakiye görüntüle ");
+            System.out.println("2. Para Yatır");
+            System.out.println("3. Para Çek");
+            System.out.println("4. Çıkış");
+            System.out.print("Lütfen Bir İşlem Seçin :");
+
+            int secim = sc.nextInt();
+
+        }
     }
 }
