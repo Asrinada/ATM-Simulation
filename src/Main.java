4d54980bd93d4619ff0532a6eef50c740b6486a5
Asrinada <adakolbasi@gmail.com>
2026-01-19 12:55:38 +0300

    initialize bank name and starting balance

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        double bakiye = 1000.0;
+        boolean calisiyorMu = true;
+
+        System.out.println("--EKONOMİ BANKASI ATM'sine HOŞGELDİNİZ--");
+    }
+}
